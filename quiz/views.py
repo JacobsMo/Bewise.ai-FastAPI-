c82d93ab5171bf6882f30ad696b1b5d1ea6620e8
Jacob Gulyaev <jgulyaev@bk.ru>
2023-10-18 00:10:05 +0700

    Добавил инструкцию к проекту, исправил ендпоинт.

--- quiz/views.py
+++ quiz/views.py
@@ -13,7 +13,7 @@ from .services import add_question
 
 
 quiz_router = APIRouter(
-    prefix='/quiz',
+    prefix='/api/quiz',
     tags=['Quiz'],
 )
 
@@ -23,7 +23,7 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(DEFAULT_HANDLER)
 
 
-@quiz_router.post('/get', response_model=QuestionsOutput)
+@quiz_router.post('/get_questions', response_model=QuestionsOutput)
 async def get_questions(questions_count: QuestionsCount) -> Response:
     questions = await add_question(questions_count.questions_count)
     response = QuestionsOutput(**{'questions': [dict([('id', question[0]), ('question', question[1])]) for question in questions]})
